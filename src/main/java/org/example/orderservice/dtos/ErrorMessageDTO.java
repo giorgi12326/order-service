4298eb071b1af4da22e554fbfd3b42a09b675741
jgushiann <njgus21@freeuni.edu.ge>
2025-11-18 14:01:23 +0400

    updated http statuses

---
+++ src/main/java/org/example/orderservice/dtos/ErrorMessageDTO.java
@@ -0,0 +1,14 @@
+package org.example.orderservice.dtos;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+@Data
+@Builder
+public class ErrorMessageDTO {
+    String message;
+    HttpStatus status;
+    LocalDateTime timestamp;
+}
